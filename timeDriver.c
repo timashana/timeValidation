771b6d18d3433a0fa3fefe180ad7341ec372da1a
Anastasiia Timashova <anastimashova@gmail.com>
2022-07-12 18:18:43 -0400

    Initial skeleton for libraries and the main driver

---
+++ timeDriver.c
@@ -0,0 +1,17 @@
+#include "./ht.h"
+#include "./iso8801.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+int main()
+{
+    // open in_file, out_file
+    // read values from in_file
+    // for each value:
+    //   check: 1.ISO 8801, 2.uniqueness, 
+    //   wite to file
+    // close files
+}
